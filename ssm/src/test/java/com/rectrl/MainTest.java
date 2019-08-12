46b28f06ff5136c4580396096dd5158f78d836fb
zhanghongen <12345678>
2019-08-12 20:34:53 +0800

    第一次

--- ssm/src/test/java/com/rectrl/MainTest.java
+++ ssm/src/test/java/com/rectrl/MainTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:META-INF/applicationContext.xml"})
 public class MainTest {
@@ -20,6 +22,21 @@ public class MainTest {
         long bookId = 1000;
         Book book = bookRepository.queryById(bookId);
         System.out.println(book);
+    }
+
+    @Test
+    public void testQueryAll() throws Exception {
+        List<Book> books = bookRepository.queryAll(0, 4);
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    @Test
+    public void testReduceNumber() throws Exception {
+        long bookId = 1000;
+        int update = bookRepository.reduceNumber(bookId);
+        System.out.println("update=" + update);
     }
 
 
